94f5a041cf2b888f68e9aa104978728289c36ea9
John Knotts <jhnnyk@gmail.com>
2024-09-04 14:46:39 -0600

    implement the + and * operators

--- point.dart
+++ point.dart
@@ -10,8 +10,17 @@ class Point {
   bool operator ==(covariant Point other) {
     return x == other.x && y == other.y;
   }
+
+  Point operator +(Point other) {
+    return Point(x + other.x, y + other.y);
+  }
+
+  Point operator *(int other) {
+    return Point(x * other, y * other);
+  }
 }
 
 void main() {
-  print(Point(1, 1) == Point(1, 1));
+  print(Point(1, 1) + Point(2, 0));
+  print(Point(2, 1) * 5);
 }
